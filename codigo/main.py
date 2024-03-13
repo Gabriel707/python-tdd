d439707ce7ec20517da62ccdd6cdb70553add497
Gabriel Araujo <gabrielsant95@gmail.com>
2024-03-12 21:22:23 -0300

    Starting tests with pytest

--- codigo/main.py
+++ codigo/main.py
@@ -2,7 +2,13 @@ from codigo.bytebank import Funcionario
 
 
 def teste_idade():
-    funcionario_teste = Funcionario('Teste', '03/02/1995', 1111)
+    funcionario_teste = Funcionario('Teste', '03/02/2000', 1111)
+    print(f'Teste = {funcionario_teste.idade()}')
+
+    funcionario_teste = Funcionario('Teste', '03/02/1999', 1111)
+    print(f'Teste = {funcionario_teste.idade()}')
+
+    funcionario_teste = Funcionario('Teste', '03/12/1995', 1111)
     print(f'Teste = {funcionario_teste.idade()}')
 
 teste_idade()
